f8b2e18d9eb24bc68c22fdaecbcf96234d6232fd
zergar <sch.d.handy@gmail.com>
2016-05-10 21:18:24 +0200

    polished design, added persistent db

--- app/assets/js/fillEdit.js
+++ app/assets/js/fillEdit.js
@@ -1,15 +1,34 @@
 $.get("/todos", function(todos) {
 	$.each(todos, function(index, product) {
-										$('#todos').append($("<tr>"));
-										$("tr:last").last().append("<td>" + "<input type=\"text\" name=\"description\" value=\"" + product.description + "\" id=\"descr-" + product.id + "\"></input>" + "</td>");
-										$("tr:last").last().append("<td>" + "<input type=\"date\" name=\"date\" value=\"" + product.date + "\" id=\"date-" + product.id + "\"></input>" + "</td>");
-										$("tr:last").last().append("<td>" + "<input type=\"progress\" min=\"0\" max=\"100\" step=\"5\" name=\"progress\" value=\"" + product.progress + "\" id=\"progr-" + product.id + "\"></input>" + "</td>");
-										$("tr:last").last().append("<td> " + " <button type=\"button\" class=\"btn btn-default\" onclick=\" doUpdate(" + product.id + ")\"> <span class=\"glyphicon glyphicon-pencil\" aria-hidden=\"true\"></span> </button>" + 
-												" <button type=\"button\" class=\"btn btn-default\" onClick=\" $.ajax({url:'/deleteTodo/" + product.id + "', type: 'DELETE', success: location.reload()});\"> <span class=\"glyphicon glyphicon-remove\" aria-hidden=\"true\"></span>  </button>" + "</td>");
-										$("tr:last").last().append(" </form>");
-
-									});
-				});
+		$('#todos').append($("<tr>"));
+		$("tr:last").last().append("" +
+			"<td class=\"row\">" +
+				"<form action=\"@routes.HomeController.addTodo()\" method=\"post\">" +
+					"<div class=\"col-sm-4 col-xs-12\">" +
+						"<input type=\"text\" name=\"description\" value=\"" + product.description + "\" id=\"descr-" + product.id + " class=\"form-control input-group\" style=\"width:100%\"></input>" + 
+					"</div>" + 
+					"<div class=\"col-sm-8 col-xs-12\">" +
+						"<div class=\"row\">" +
+							"<div class=\"col-xs-4\">" +
+								"<input type=\"date\" name=\"date\" value=\"" + product.date + "\" id=\"date-" + product.id + "\" class=\"form-control input-group\"></input>" + 
+							"</div>" +
+							"<div class=\"col-xs-4\">" +
+								"<input type=\"range\" min=\"0\" max=\"100\" step=\"5\" name=\"progress\" value=\"" + product.progress + "\" id=\"progr-" + product.id + "\" class=\"form-control input-group\"></input>" + 
+							"</div>" +
+							"<div class=\"row col-xs-4\">" +
+								"<button type=\"button\" class=\"btn btn-default\" onclick=\" doUpdate(" + product.id + ")\"> " +
+									"<span class=\"glyphicon glyphicon-pencil\" aria-hidden=\"true\"></span> " +
+								"</button>" + 
+								"<button type=\"button\" class=\"btn btn-default\" onClick=\" $.ajax({url:'/deleteTodo/" + product.id + "', type: 'DELETE', success: location.reload()});\"> " +
+									"<span class=\"glyphicon glyphicon-remove\" aria-hidden=\"true\"></span>  " +
+								"</button>" + 
+							"</div>" +
+						"</div>" +
+					"</div>" +
+				"</form>" +
+			"</td>");
+	});
+});
 
 
 function doUpdate(id){
--- app/assets/js/showEntry.js
+++ app/assets/js/showEntry.js
@@ -1,11 +1,25 @@
 $.get("/todos",function(todos) {
 	$.each(todos,function(index, product) {
 		$('#todos').append($("<tr>"));
-		$("tr:last").last().append("<td>" + product.description + "</td>");
-		$("tr:last").last().append("<td>" + product.date + "</td>");
-		$("tr:last").last().append("<td>" + product.progress + "</td>");
-		$("tr:last").last().append("<td>" + "<button  type=\"button\" class=\"btn btn-default\" onclick=\" window.location.href = '/editTodo.html'\"> <span class=\"glyphicon glyphicon-pencil\" aria-hidden=\"true\"></span> </button> " +
-		" <button type=\"button\" class=\"btn btn-default\" onClick=\" $.ajax({url:'/deleteTodo/" + product.id + "', type: 'DELETE', success: location.reload()});\"> <span class=\"glyphicon glyphicon-remove\" aria-hidden=\"true\"></span>  </button>" + "</td>");
+		$("tr:last").last().append("" +
+				"<td class=\"row\">" +
+					"<div class=\"col-sm-6 col-xs-12\" style=\"font-size:18pt\">" + product.description + "</div>" +
+					"<div class=\"col-sm-6 col-xs-12\">" +
+						"<div class=\"row\">" +
+							"<div class=\"col-xs-6\" style=\"font-size:18pt\">" + product.date + "</div>" +
+							"<div class=\"col-xs-3\" style=\"font-size:18pt\">" + product.progress + "%</div>" +
+							"<div class=\"row col-xs-3\">" +
+								"<button  type=\"button\" class=\"btn btn-default\" onclick=\" window.location.href = '/editTodo.html'\">" +
+									"<span class=\"glyphicon glyphicon-pencil\" aria-hidden=\"true\"></span>" +
+								"</button> " +
+								"<button type=\"button\" class=\"btn btn-default\" onClick=\" $.ajax({url:'/deleteTodo/" + product.id + "', type: 'DELETE', success: location.reload()});\">" +
+									"<span class=\"glyphicon glyphicon-remove\" aria-hidden=\"true\"></span>" +
+								"</button>" +
+							"</div>" +
+						"</div>" +
+					"</div>" +
+				"</td>");
+
 	});
 });
 /*
