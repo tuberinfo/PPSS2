f20c79c6ade8f78ed4fc19035ea63641133f5f73
zergar <sch.d.handy@gmail.com>
2016-05-10 17:52:58 +0200

    small changes for comfort

--- app/assets/js/showEntry.js
+++ app/assets/js/showEntry.js
@@ -4,7 +4,7 @@ $.get("/todos",function(todos) {
 		$("tr:last").last().append("<td>" + product.description + "</td>");
 		$("tr:last").last().append("<td>" + product.date + "</td>");
 		$("tr:last").last().append("<td>" + product.progress + "</td>");
-		$("tr:last").last().append("<td>" + "<button  type=\"button\" class=\"btn btn-default\" onclick=\" $.ajax({url:'/changeTodo/" + product.id + "', type: 'PUT', success: location.reload()});\"> <span class=\"glyphicon glyphicon-pencil\" aria-hidden=\"true\"></span> </button> " +
+		$("tr:last").last().append("<td>" + "<button  type=\"button\" class=\"btn btn-default\" onclick=\" window.location.href = '/editTodo.html'\"> <span class=\"glyphicon glyphicon-pencil\" aria-hidden=\"true\"></span> </button> " +
 		" <button type=\"button\" class=\"btn btn-default\" onClick=\" $.ajax({url:'/deleteTodo/" + product.id + "', type: 'DELETE', success: location.reload()});\"> <span class=\"glyphicon glyphicon-remove\" aria-hidden=\"true\"></span>  </button>" + "</td>");
 	});
 });
